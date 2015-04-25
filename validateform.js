306f3f1eb4ef5d46b9d30af33e2a114ec44471f8
benwedin <wedinb@carleton.edu>
2015-04-25 16:55:43 -0500

    significant changes by Ben

---
+++ validateform.js
@@ -0,0 +1,15 @@
+function validateForm() {
+    var numer = document.forms["myForm"]["numer"].value;
+  	var denom = document.forms["myForm"]["denom"].value;
+  	var mod = document.forms["myForm"]["mod"].value;
+    if (numer == (null || "") || numer < 1 ||
+         denom == (null || "") || denom < 1 ||
+         mod == (null || "") || mod < 1) {
+        alert("All fields must be filled out");
+        return false;
+    }
+  	if (denom <= numer) {
+        alert("Please make numerator less than the denominator (for aesthetic reasons)");
+        return false;
+    }
+}
