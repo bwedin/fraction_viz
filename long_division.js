9294db9ad335757c59cd08136272850c84ef0b4f
Marielle Foster <marielleefoster@gmail.com>
2015-04-25 12:41:13 -0500

    LONG DIVISION
    in any base

--- long_division.js
+++ long_division.js
@@ -1,3 +1,13 @@
-function long_division(n, mod){
-	
+function long_division(n, d, mod){
+	var remainders = [];
+	var ptemp = n;
+	var qtemp = -4;
+	for(i= 0; i < d; i++){
+		qtemp = (ptemp*mod) % d;
+		remainders.push(qtemp);
+		ptemp = qtemp;
+	}
+	return remainders;
 }
+
+//console.log(long_division(1, 101, 103))
